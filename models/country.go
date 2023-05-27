1763891a6a80c666c4ba86bdd1c35fa65b5fa653
Syahril Ramadhan <rodyulo20@gmail.com>
2023-05-27 09:59:18 +0700

    Cek

--- models/country.go
+++ models/country.go
@@ -3,9 +3,8 @@ package models
 import "time"
 
 type Country struct {
-	Id   int    `json:"id_country" form:"id_country" gorm:"primary_key:auto_increment"`
-	Name string `json:"name_country" form:"name" gorm:"type: varchar(255)"`
-	// User      UserResponse `json:"-"`
+	Id        int       `json:"id_country" form:"id_country" gorm:"primary_key:auto_increment"`
+	Name      string    `json:"name_country" form:"name" gorm:"type: varchar(255)"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
